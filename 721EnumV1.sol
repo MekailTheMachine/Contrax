3e5b68327061260aa8c1957b106c819c6f6a7877
mekail <mekailmurphy@gmail.com>
2023-02-08 09:50:00 -0700

    Dynamic ERC-721 Contract :D

--- 721EnumV1.sol
+++ 721EnumV1.sol
@@ -14,21 +14,18 @@ contract NFT is ERC721Enumerable, Ownable {
   // Variables
   string baseURI; // base URI that will be used to generate the full token URI
   string public baseExtension = ".json"; // file extension that will be used to generate the full token URI
-
-  uint256 public maxSupply = 1528; // maximum number of tokens that can be minted
-  uint256 public maxMintAmountPerTx = 20; // maximum number of tokens that can be minted in one transaction
-  uint256 public currentSupply; // the current number of tokens in circulation
-  uint256 public costFlux = 1 + (currentSupply/ maxSupply); // the rate at which the cost of minting changes
-  uint256 public cost = (1.95 * costFlux) * 10**16; // the cost of minting tokens
-  uint256 public maxMintAmount;
-
-  // uint256[] public usedTokenIds; // an array that will be used in version 2.0 to randomize TokenIDs
+  string public notRevealedUri; // the URI to be returned if the metadata of the tokens is not revealed
+  
+  uint256 public maxSupply = 1528;
+  uint256 public maxMintAmount = 20;
+  uint256 public currentSupply = 0;
+  uint256 public costFlux = 1 + (currentSupply/ maxSupply);
+  uint256 public cost = 0.0195 ether;
 
   bool public paused = true;
-  bool public revealed = false; 
-  string public notRevealedUri; // the URI to be returned if the metadata of the tokens is not revealed
+  bool public revealed = false;
 
-  // Contract constructor
+// Contract constructor
   constructor(
     string memory _name, 
     string memory _symbol, 
@@ -46,45 +43,28 @@ contract NFT is ERC721Enumerable, Ownable {
     return baseURI;
   }
 
-  // ========== Mint function ==========================================================================================
+  // ======================== MINT FUNCTION ==================================================
   function mint(uint256 _mintAmount) public payable {
-    // Check if the contract is paused and the mint amount is more than zero
+    uint256 supply = totalSupply();
     require(!paused, "The contract is currently paused");
     require(_mintAmount > 0, "Invalid mint amount");
-    require(_mintAmount <= maxMintAmountPerTx, "Exceeded max mint amount per transaction");
-
-    // Check if the sender of the transaction is not the contract owner
-    if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount, "Insufficient funds");
-    }
-    uint256 supply = totalSupply();
-    require(supply + _mintAmount <= maxSupply, "Max supply exceeded");
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      // Mint the tokens and add them to the sender's balance
-      _safeMint(msg.sender, supply + i);
-    }
-
+    require(_mintAmount <= maxMintAmount);
     require(supply + _mintAmount <= maxSupply);
 
     if (msg.sender != owner()) {
-      // Check if the value of the message sent by the sender is greater than or equal to the cost of minting
-      require(msg.value >= cost * _mintAmount);
+      require(msg.value >= cost * costFlux * _mintAmount);
     }
 
-    // Loop to mint the specified number of tokens
     for (uint256 i = 1; i <= _mintAmount; i++) {
-      // Mint a token and assign it to the sender of the message
       _safeMint(msg.sender, supply + i);
+      currentSupply += 1;
     }
 
-    // Update the current supply and costFlux variables
-    currentSupply += _mintAmount;
     costFlux = 1 + (currentSupply / maxSupply);
-    cost = 1.95 * 10**16 * costFlux;
-    
+      // mint tokens, and update the cost flux
   }
 
-// ============================= OTHER PUBLIC FUNCTIONS ===============================================================
+  // ============================= OTHER PUBLIC FUNCTIONS ===============================================================
   // Function to get the token IDs of all tokens owned by a particular address
   function walletOfOwner(address _owner) public view returns (uint256[] memory) {
     // Get the total number of tokens owned by the address
@@ -102,7 +82,7 @@ contract NFT is ERC721Enumerable, Ownable {
     return tokenIds;
   }
 
-  // Function to get the token URI for a specific token ID
+  // TokenURI - TokenID pairing
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
     // Check if the token ID exists
     require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
